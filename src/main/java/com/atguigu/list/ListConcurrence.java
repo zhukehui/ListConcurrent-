546ade7395a32bfca61c041e075e479372b69f2d
zhukehui <574059694@qq.com>
2019-10-11 20:22:50 +0800

    List多线程高并发示例

--- src/main/java/com/atguigu/list/ListConcurrence.java
+++ src/main/java/com/atguigu/list/ListConcurrence.java
@@ -7,7 +7,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class ListConcurrence {
 
 	public static void main(String[] args) {
-		List<String> list =new CopyOnWriteArrayList<String>();
+		List<String> list = new CopyOnWriteArrayList<String>();
 		/*
 		 * list.add("a"); list.add("b"); list.add("c");
 		 * 
@@ -16,22 +16,27 @@ public class ListConcurrence {
 
 		/*
 		 * java.util.ConcurrentModificationException(并发修改异常) 
-		 * 解决list多线程并发修改异常:
-		 * 1.new Vector
+		 * 解决list多线程并发修改异常: 
+		 * 1.newVector 
 		 * 2.Collections.synchronizedList(new ArrayList<>())
 		 * 3.new CopyOnWriteArrayList<String>()
+		 * 
+		 * Lambda表达式格式：拷贝小括号     固定小箭头     落地大括号
 		 */
 
 		for (int i = 0; i < 30; i++) {
-			new Thread(new Runnable() {
-
-				@Override
-				public void run() {
-					list.add(UUID.randomUUID().toString().substring(0, 6));
-					System.out.println(list);
-				}
+			new Thread(() -> {
+				list.add(UUID.randomUUID().toString().substring(0, 6));
+				System.out.println(list);
 			}, String.valueOf(i)).start();
 		}
+		/*
+		 * for (int i = 0; i < 30; i++) { new Thread(new Runnable() {
+		 * 
+		 * @Override public void run() {
+		 * list.add(UUID.randomUUID().toString().substring(0, 6));
+		 * System.out.println(list); } }, String.valueOf(i)).start(); }
+		 */
 
 	}
 }
